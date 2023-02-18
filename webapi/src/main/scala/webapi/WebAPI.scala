a468bf17eaac5fbf60f06cb4ebe94410fa14a730
rthoth! <ronaldo.asilva@gmail.com>
2023-02-17 21:26:54 -0300

    [webapi] chore: start.

--- webapi/src/main/scala/webapi/WebAPI.scala
+++ webapi/src/main/scala/webapi/WebAPI.scala
@@ -17,6 +17,6 @@ object WebAPI extends ZIOAppDefault:
       farangoModule   <- FarangoModule(config.arango)
       kafkaManager    <- KafkaModule(config.kafka)
       httpModuleLayer <- HttpModule(config.http)
-      _               <- ZIO.logInfo("🐧 Tremors WebAPI was ready, grab your popcorn, soda and enjoy it!")
+      _               <- ZIO.logInfo("🐧 Tremors WebAPI is ready. Grab your popcorn, soda and enjoy it!")
       exit            <- httpModuleLayer.launch
     yield exit
