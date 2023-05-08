86fb60f5562607e911fe86cf2c0b294ede148a1b
rthoth! <ronaldo.asilva@gmail.com>
2023-05-08 20:47:44 -0300

    [web] poc: animation + rotation.

--- web/utils/itowns/EarthQuakeLayer.ts
+++ web/utils/itowns/EarthQuakeLayer.ts
@@ -1,6 +1,8 @@
 
 const THREE = itowns.THREE
 
+const axis = new THREE.Vector3(1, 0, 0).normalize()
+
 export class EarthquakeLayer extends itowns.Layer {
 
   constructor(id: string, options: Record<string, any>) {
@@ -19,19 +21,19 @@ export class EarthquakeLayer extends itowns.Layer {
   }
 
   onAdd(view: GlobeView) {
-    this.geometry = new THREE.SphereGeometry(100000)
+    const size = 200000
     this.material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-    this.sphere = new THREE.Mesh(this.geometry, this.material)
+    this.cubeGeometry = new THREE.BoxGeometry(size, size, size)
+    this.cubeMesh = new THREE.Mesh(this.cubeGeometry, this.material)
+    this.cubeMesh.translateZ(7000000)
 
+    this.orbit = new THREE.Object3D()
+    this.orbit.add(this.cubeMesh)
 
-    var target = itowns.CameraUtils.getTransformCameraLookingAtTarget(view, view.camera.camera3D).coord;
-    this.sphere.altitude = 9000
-    this.sphere.position.copy(target.as(view.referenceCrs))
-    console.log(this.sphere.position)
-    this.sphere.updateMatrixWorld()
-    this.object3d.add(this.sphere)
-    this.scale = 1
+    this.orbit.updateMatrixWorld()
+    this.angle = 0
 
+    this.object3d.add(this.orbit)
     view.addFrameRequester(itowns.MAIN_LOOP_EVENTS.BEFORE_RENDER, this._frameAnimation)
     this.view = view;
     view.notifyChange()
@@ -39,17 +41,15 @@ export class EarthquakeLayer extends itowns.Layer {
 
   onRemove(view: GlobeView) {
     view.removeFrameRequester(itowns.MAIN_LOOP_EVENTS.BEFORE_RENDER, this._frameAnimation)
-    this.object3d.remove(this.sphere)
+    this.object3d.remove(this.orbit)
   }
 
   #frameAnimation() {
-    if (this.sphere) {
-      this.sphere.scale.set(this.scale, this.scale, this.scale)
-      this.sphere.updateMatrixWorld()
-      this.scale = (this.scale + 0.3) % 200
-      console.log(this.scale)
-      this.view.notifyChange()
-    }
+    this.orbit.rotateX(0.01)
+    this.cubeMesh.rotateY(0.01)
+    this.angle = (this.angle + 0.01) % Math.PI
+    this.orbit.updateMatrixWorld()
+    this.view.notifyChange()
   }
 }
 
