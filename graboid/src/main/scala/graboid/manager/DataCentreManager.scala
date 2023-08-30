6226f13cf7e602e54eac6227591e129e916f967b
rthoth! <ronaldo.asilva@gmail.com>
2023-08-29 21:47:19 -0300

    [graboid] chore: remove unnecessary repositories.

--- graboid/src/main/scala/graboid/manager/DataCentreManager.scala
+++ graboid/src/main/scala/graboid/manager/DataCentreManager.scala
@@ -2,11 +2,9 @@ package graboid.manager
 
 import com.softwaremill.macwire.wire
 import graboid.DataCentre
-import graboid.logDebug
-import graboid.logErrorCause
-import graboid.logInfo
 import graboid.repository.DataCentreRepository
 import zio.Task
+import zio.ZIO
 import zio.stream.ZStream
 
 trait DataCentreManager:
@@ -30,26 +28,26 @@ object DataCentreManager:
     override def add(dataCentre: DataCentre): Task[DataCentre] =
       repository
         .insert(dataCentre)
-        .tap(logInfo(s"The Data Centre ${dataCentre.id} was added."))
-        .tapErrorCause(logErrorCause(s"It was impossible to add data centre: ${dataCentre.id}!"))
+        .tap(_ => ZIO.logInfo(s"The Data Centre ${dataCentre.id} was added."))
+        .tapErrorCause(ZIO.logErrorCause(s"It was impossible to add data centre: ${dataCentre.id}!", _))
 
     override def update(dataCentre: DataCentre): Task[DataCentre] =
       repository
         .update(dataCentre)
-        .tap(logDebug(s"The Data Centre ${dataCentre.id} was updated."))
-        .tapErrorCause(logErrorCause(s"It was impossible to update the data centre ${dataCentre.id}!"))
+        .tap(_ => ZIO.logDebug(s"The Data Centre ${dataCentre.id} was updated."))
+        .tapErrorCause(ZIO.logErrorCause(s"It was impossible to update the data centre ${dataCentre.id}!", _))
 
     override def delete(id: String): Task[DataCentre] =
       repository
         .delete(id)
-        .tap(logDebug(s"The Data Centre $id was removed."))
-        .tapErrorCause(logErrorCause(s"It was impossible to delete the data centre $id!"))
+        .tap(_ => ZIO.logDebug(s"The Data Centre $id was removed."))
+        .tapErrorCause(ZIO.logErrorCause(s"It was impossible to delete the data centre $id!", _))
 
     override def get(id: String): Task[Option[DataCentre]] =
       repository
         .get(id)
-        .tapErrorCause(logErrorCause(s"It was impossible to get the data centre $id!"))
+        .tapErrorCause(ZIO.logErrorCause(s"It was impossible to get the data centre $id!", _))
 
     override def all: ZStream[Any, Throwable, DataCentre] =
       repository.all
-        .tapErrorCause(logErrorCause("It was impossible to get the list of data centres!"))
+        .tapErrorCause(ZIO.logErrorCause("It was impossible to get the list of data centres!", _))
--- graboid/src/main/scala/graboid/module/RepositoryModule.scala
+++ graboid/src/main/scala/graboid/module/RepositoryModule.scala
@@ -1,52 +1,24 @@
 package graboid.module
 
-import com.arangodb.model.GeoIndexOptions
-import com.arangodb.model.PersistentIndexOptions
 import com.softwaremill.macwire.Module
 import com.softwaremill.macwire.wire
 import graboid.repository.DataCentreRepository
-import graboid.repository.EventRepository
-import graboid.repository.HypocentreRepository
-import one.estrondo.farango.IndexDescription
 import tremors.zio.farango.FarangoModule
 import zio.Task
 
 @Module
 trait RepositoryModule:
 
-  def hypocentreRepository: HypocentreRepository
-
-  def eventRepository: EventRepository
-
   def dataCentreRepository: DataCentreRepository
 
 object RepositoryModule:
 
   def apply(farangoModule: FarangoModule): Task[RepositoryModule] =
-    for
-      hypocentreRepository <- farangoModule
-                                .collection(
-                                  "hypocentre",
-                                  Seq(
-                                    IndexDescription.Geo(Seq("centre"), GeoIndexOptions().geoJson(true))
-                                  )
-                                )
-                                .map(HypocentreRepository.apply)
-      eventRepository      <- farangoModule
-                                .collection(
-                                  "event",
-                                  Seq(
-                                    IndexDescription.Persistent(Seq("id"), PersistentIndexOptions().unique(true))
-                                  )
-                                )
-                                .map(EventRepository.apply)
-      dataCentreRepository <- farangoModule
-                                .collection("data-centre")
-                                .map(DataCentreRepository.apply)
+    for dataCentreRepository <- farangoModule
+                                  .collection("data-centre")
+                                  .map(DataCentreRepository.apply)
     yield wire[Impl]
 
   private class Impl(
-      val hypocentreRepository: HypocentreRepository,
-      val eventRepository: EventRepository,
       val dataCentreRepository: DataCentreRepository
   ) extends RepositoryModule
