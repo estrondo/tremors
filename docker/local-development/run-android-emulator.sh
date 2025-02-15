d760ad396a5aa52888555e407af75f1f328d7690
rthoth! <ronaldo.asilva@gmail.com>
2025-02-15 09:58:21 -0300

    local environment adjusts.

--- docker/local-development/run-android-emulator.sh
+++ docker/local-development/run-android-emulator.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOMAINS="local.estrondo.one,local.tremors-api.estrondo.one"
+DOMAINS="local.estrondo.one,local-tremors-api.estrondo.one"
 
 case $1 in
   "")
@@ -8,16 +8,20 @@ case $1 in
       emulator -list-avds
       ;;
   *)
+      echo "I need your root power!"
+      sudo echo "Starting DNS Server..."
       sudo dnsmasq -d -h --host-record=$DOMAINS,10.0.2.2 &
       sleep 3
 
       # reference: https://stackoverflow.com/questions/16965089/getting-pid-of-process-in-shell-script
-      DNS_SERVER_PID=`/bin/ps -fu root | grep -v "sudo" | grep "dnsmasq" | grep -v "grep" | awk '{print $2}'`
+      DNS_MASQ_PID=`/bin/ps -fu root | grep -v "sudo" | grep "dnsmasq" | grep -v "grep" | awk '{print $2}'`
 
-      echo "DNS Server has been started with PID=$DNS_SERVER_PID"
+      echo "dnsmasq PID=$DNS_MASQ_PID"
+      sleep 3
       emulator "@$1" -dns-server 127.0.0.1
 
-      echo "killing DNS Server..."
-      sudo kill -9 "$DNS_SERVER_PID"
+      echo "I'm about to kill dnsmasq"
+      sleep 3
+      sudo kill -9 "$DNS_MASQ_PID"
       ;;
 esac
